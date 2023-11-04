c30c66fe35920c1c86066fab71fda9259edbd9b7
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-04 19:29:02 -0400

    Adds styling for navbar links

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,6 +24,10 @@ export const links = [
     name: "Experience",
     section: "#experience",
   },
+  {
+    name: "Contact",
+    section: "#contact",
+  },
 ];
 
 const Header = () => {
@@ -35,10 +39,18 @@ const Header = () => {
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
       <nav className="fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[2rem] sm:h-[initial] sm:py-0">
-        <ul>
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 font-medium text-gray-600 sm:w-[initial] sm:flex-nowrap sm:gap-3">
           {links.map((link) => (
-            <li key={link.section}>
-              <Link href={link.section}>{link.name}</Link>
+            <li
+              className="h-3/4 flex items-center justify-center"
+              key={link.section}
+            >
+              <Link
+                className="flex w-full items-center justify-center px-3 py-1 hover:text-gray-800 transition"
+                href={link.section}
+              >
+                {link.name}
+              </Link>
             </li>
           ))}
         </ul>
