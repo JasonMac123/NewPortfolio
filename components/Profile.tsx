2cc12abd42b3c16552d75934f8ce0aef742ae767
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-04 19:39:44 -0400

    Updates next image component with src photo

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,7 +1,18 @@
 "use client";
 
+import Image from "next/image";
+import JasonPhoto from "../public/images/JasonPhoto.jpg";
+
 const Profile = () => {
-  return <section></section>;
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div>
+          <Image src={JasonPhoto} alt="Profile picture of Jason" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
