86ae63a9c1f4207de7834ea86db10b281c6403a1
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-04 19:32:48 -0400

    Adds motion to the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -41,9 +41,11 @@ const Header = () => {
       <nav className="fixed flex top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[2rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 font-medium text-gray-600 sm:w-[initial] sm:flex-nowrap sm:gap-3">
           {links.map((link) => (
-            <li
+            <motion.li
               className="h-3/4 flex items-center justify-center"
               key={link.section}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
             >
               <Link
                 className="flex w-full items-center justify-center px-3 py-1 hover:text-gray-800 transition"
@@ -51,7 +53,7 @@ const Header = () => {
               >
                 {link.name}
               </Link>
-            </li>
+            </motion.li>
           ))}
         </ul>
       </nav>
