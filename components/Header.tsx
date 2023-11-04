c0c018c8a32c68e6c7394660ca9e9ea48902febb
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-04 19:23:51 -0400

    Iterates over links and adds redirection to portfolio section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,28 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 export const links = [
   {
     name: "Home",
-    hash: "#home",
+    section: "#home",
   },
   {
     name: "About",
-    hash: "#about",
+    section: "#about",
   },
   {
     name: "Projects",
-    hash: "#projects",
+    section: "#projects",
   },
   {
     name: "Skills",
-    hash: "#skills",
+    section: "#skills",
   },
   {
     name: "Experience",
-    hash: "#experience",
+    section: "#experience",
   },
 ];
 
@@ -34,7 +35,13 @@ const Header = () => {
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
       <nav className="fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[2rem] sm:h-[initial] sm:py-0">
-        <ul></ul>
+        <ul>
+          {links.map((link) => (
+            <li key={link.section}>
+              <Link href={link.section}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
   );
