25ea68a6bd331a3a886f9345813d019f29580df4
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-06 15:02:04 -0500

    Adds feature to download resume from download anchor tag

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -58,7 +58,11 @@ const Profile = () => {
         >
           Online Resume
         </a>
-        <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition cursor-pointer">
+        <a
+          href="/Jason-Mac-Resume-Nov2023.pdf"
+          download
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition cursor-pointer"
+        >
           Download Resume{" "}
           <AiOutlineDownload
             size={20}
