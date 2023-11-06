ccabaae8cf4fe55d08cd59b65b9a6753816dafa9
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-05 21:58:52 -0500

    Adds ProfileDescription

--- components/ProfileDescription.tsx
+++ components/ProfileDescription.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 const ProfileDescription = () => {
-  return <p></p>;
+  return (
+    <p>
+      Hey I&lsquo;m Jason, a full-stack developer marking my journery into web
+      development. My expertise is in typescript, javascript, react, next, and
+      building web applications.
+    </p>
+  );
 };
 
 export default ProfileDescription;
