12c762225b3ed8fe197c2abd637eda4e2df381e0
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-06 15:42:37 -0500

    Adds about section and imports to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/About";
 import Profile from "@/components/Profile";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Profile />
+      <About />
     </main>
   );
 }
