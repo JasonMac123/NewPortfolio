7d67c314ac676d4c9a718cfd7b87bca1d8103b82
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-05 22:10:46 -0500

    Centers content within homepage and adds motion

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Profile from "@/components/Profile";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col items-center px-4">
       <Profile />
     </main>
   );
