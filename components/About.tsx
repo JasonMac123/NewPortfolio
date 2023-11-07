44cd4e59dcecea0e2430590725663087d38ab882
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-07 15:13:47 -0500

    Adds about section information

--- components/About.tsx
+++ components/About.tsx
@@ -4,6 +4,21 @@ const About = () => {
   return (
     <section>
       <h2>About me</h2>
+      <p>
+        I graduated with a degree in <span>Chemistry</span>, after spending a
+        year in the industry. I decided to pursue programming. I{" "}
+        <span>
+          learned and developed my skills in full-stack web developing
+        </span>{" "}
+        after enrolling in Lighthouse Labs. The reason why I pursued programming
+        was that I loved creative aspect of turning my ideas into software or
+        websites and the puzzle/problem solving side of programming. My core web
+        development skills is{" "}
+        <span>React, typescript, javascript, HTML, and CSS</span>. I am familiar
+        with Java and .NET. I am always looking to learn and develop my skills,
+        but if I&apos;m not coding then you can probably find me playing
+        volleyball or at a sports game.
+      </p>
     </section>
   );
 };
