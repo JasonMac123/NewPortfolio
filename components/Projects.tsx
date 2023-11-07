95b66fe1622ebee3037e118c89b80caaf4f831ff
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-07 16:28:17 -0500

    Creates project data to display

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,6 +2,30 @@
 
 import SectionTitle from "./SectionTitle";
 
+const projectData = [
+  {
+    title: "Animal-Pals",
+    description:
+      "A web-application where users to rent their houses to take care of other people pets, or find a house to house their pet while on a vacation.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma", "NextAuth"],
+    image: "",
+  },
+  {
+    title: "Look of Japan",
+    description:
+      "A E-commerce site where users can find souvernirs from all over Asia from countries like Japan, China, Korea, and order them.",
+    tags: ["React", "Next.js", "Firebase", "Tailwind", "Axios"],
+    image: "",
+  },
+  {
+    title: "AniShare",
+    description:
+      "A social media website where anime users can discuss, post images, and talk about anime. Users can follow other users, like posts, and comment.",
+    tags: ["React", "Next.js", "MySql", "Tailwind", "Prisma", "Planetscape"],
+    image: "",
+  },
+];
+
 const Projects = () => {
   return (
     <section>
