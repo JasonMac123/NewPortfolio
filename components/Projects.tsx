f33fa11ee373f3c3553ca987ed2315f594433ae5
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-07 18:07:00 -0500

    Updates projects with photos

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import SectionTitle from "./SectionTitle";
+import AniSharePhoto from "../public/images/AniShare.jpg";
+import AnimalPalPhoto from "../public/images/AnimalPal.jpg";
+import LookOfJapanPhoto from "../public/images/LookOfJapan.jpg";
 
 const projectData = [
   {
@@ -8,21 +11,21 @@ const projectData = [
     description:
       "A web-application where users to rent their houses to take care of other people pets, or find a house to house their pet while on a vacation.",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma", "NextAuth"],
-    image: "",
+    image: AnimalPalPhoto,
   },
   {
     title: "Look of Japan",
     description:
       "A E-commerce site where users can find souvernirs from all over Asia from countries like Japan, China, Korea, and order them.",
     tags: ["React", "Next.js", "Firebase", "Tailwind", "Axios"],
-    image: "",
+    image: LookOfJapanPhoto,
   },
   {
     title: "AniShare",
     description:
       "A social media website where anime users can discuss, post images, and talk about anime. Users can follow other users, like posts, and comment.",
     tags: ["React", "Next.js", "MySql", "Tailwind", "Prisma", "Planetscape"],
-    image: "",
+    image: AniSharePhoto,
   },
 ];
 
