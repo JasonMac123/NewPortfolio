02fd86badefee1ff746766bfd662a6c1901b952b
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-07 15:46:18 -0500

    Fixes typo for children props to be read

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -5,7 +5,7 @@ interface SectionTitleProps {
 }
 
 const SectionTitle: React.FC<SectionTitleProps> = ({ children }) => {
-  return <h2 className="text-3xl font-medium capitalize mb-8">children</h2>;
+  return <h2 className="text-3xl font-medium capitalize mb-8">{children}</h2>;
 };
 
 export default SectionTitle;
