ce9ddbec3bd234acf957867c3e6d5dd89ca0baef
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-08 23:50:04 -0500

    Adds spacing inbetween description and tags

--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -7,11 +7,11 @@ type ProjectProps = (typeof projectData)[number];
 
 const Project = ({ title, description, tags, image }: ProjectProps) => {
   return (
-    <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative">
-      <div className="py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:w-1/2">
+    <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]">
+      <div className="py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:w-1/2 flex flex-col h-full">
         <h3 className="text-2xl font-semibold">{title}</h3>
         <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-        <ul className="flex flex-wrap mt-4 gap-2">
+        <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
           {tags.map((tag, i) => {
             return (
               <li
