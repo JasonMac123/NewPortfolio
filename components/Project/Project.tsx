770b92dbcb775e4d0ea081265acc1f45f41cae11
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-08 23:48:24 -0500

    Fixes layout of project card

--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -8,24 +8,26 @@ type ProjectProps = (typeof projectData)[number];
 const Project = ({ title, description, tags, image }: ProjectProps) => {
   return (
     <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative">
-      <h3 className="text-2xl font-semibold">{title}</h3>
-      <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-      <ul className="flex flex-wrap mt-4 gap-2">
-        {tags.map((tag, i) => {
-          return (
-            <li
-              className="bg-black/[0.7] px-3 py-2 text-[0.5rem] uppercase tracking-wider text-white rounded-3xl"
-              key={i}
-            >
-              {tag}
-            </li>
-          );
-        })}
-      </ul>
+      <div className="py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:w-1/2">
+        <h3 className="text-2xl font-semibold">{title}</h3>
+        <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+        <ul className="flex flex-wrap mt-4 gap-2">
+          {tags.map((tag, i) => {
+            return (
+              <li
+                className="bg-black/[0.7] px-3 py-2 text-[0.5rem] uppercase tracking-wider text-white rounded-3xl"
+                key={i}
+              >
+                {tag}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
       <Image
         src={image}
         alt={"Project Photo"}
-        className="absolute top-8 -right-40"
+        className="absolute top-8 -right-40 w-[28rem] rounded-t-lg shadow-lg"
       />
     </section>
   );
