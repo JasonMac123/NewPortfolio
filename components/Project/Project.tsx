658fa96d2936b3692e8479b4a4a3c864d93f201e
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-09 16:40:43 -0500

    Properly positions images alternating between each side

--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -7,8 +7,8 @@ type ProjectProps = (typeof projectData)[number];
 
 const Project = ({ title, description, tags, image }: ProjectProps) => {
   return (
-    <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[22rem]">
-      <div className="py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:w-1/2 flex flex-col h-full">
+    <section className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0">
+      <div className="pt-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:w-1/2 flex flex-col h-full group-even:ml-[21rem]">
         <h3 className="text-2xl font-semibold">{title}</h3>
         <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
         <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
@@ -27,7 +27,7 @@ const Project = ({ title, description, tags, image }: ProjectProps) => {
       <Image
         src={image}
         alt={"Project Photo"}
-        className="absolute top-8 -right-40 w-[28rem] rounded-t-lg shadow-lg h-full object-cover"
+        className="absolute top-8 -right-40 w-[30rem] rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40 h-full object-cover"
       />
     </section>
   );
