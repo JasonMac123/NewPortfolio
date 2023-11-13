1cce59baca1d8514494fd88d8d05e1eaa430f431
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-12 22:58:43 -0500

    Adds experience header and section header redirection

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,7 +1,16 @@
 "use client";
 
+import { useSectionInView } from "@/hooks/useSectionInView";
+import SectionTitle from "./SectionTitle";
+
 const Experience = () => {
-  return <section></section>;
+  const { ref } = useSectionInView("Experience", 0.5);
+
+  return (
+    <section id="experience" ref={ref}>
+      <SectionTitle>Experience</SectionTitle>
+    </section>
+  );
 };
 
 export default Experience;
