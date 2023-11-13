e177b843d598e71f8901ecb42260d4c4810a7268
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-12 22:27:10 -0500

    Adds skills array and displays them

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -3,6 +3,28 @@
 import { useSectionInView } from "@/hooks/useSectionInView";
 import SectionTitle from "./SectionTitle";
 
+export const skillsData = {
+  frontEnd: [
+    "Javascript",
+    "React",
+    "HTML",
+    "CSS",
+    "Tailwind",
+    "Bootstrap",
+    "Node.js",
+    "Prisma",
+  ],
+  backEnd: [
+    "Typescript",
+    "Python",
+    "Java",
+    "SpringBoot",
+    "Express",
+    "Ruby",
+    "Next.js",
+  ],
+};
+
 const Skills = () => {
   const { ref } = useSectionInView("Skills", 0.3);
 
@@ -13,12 +35,22 @@ const Skills = () => {
         <h3 className="text-xl font-medium capitalize mb-8 text-center">
           Front-end
         </h3>
+        <ul>
+          {skillsData.frontEnd.map((skill, i) => {
+            return <li key={i}>{skill}</li>;
+          })}
+        </ul>
       </div>
       <div>
         <h3 className="text-xl font-medium capitalize mb-8 text-center">
           {" "}
           Back-end
         </h3>
+        <ul>
+          {skillsData.backEnd.map((skill, i) => {
+            return <li key={i}>{skill}</li>;
+          })}
+        </ul>
       </div>
     </section>
   );
