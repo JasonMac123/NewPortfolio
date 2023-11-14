e91b01f6dd0707acd11aa667d0184a81ae128242
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-14 16:30:18 -0500

    Adds styling to contact form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -8,17 +8,24 @@ const Contact = () => {
   const { ref } = useSectionInView("Contact", 0.5);
 
   return (
-    <section id="contact" ref={ref} className="mb-28 sm:mb-40 scroll-mt-28">
-      <p>
+    <section
+      id="contact"
+      ref={ref}
+      className="mb-28 sm:mb-40 scroll-mt-28 w-[min(95%, 38rem)]"
+    >
+      <p className="text-gray-700">
         Please contact me at{" "}
         <a href="mailto:m.jason.2018@gmail.com" className="underline">
           my email
         </a>{" "}
         or through this form
       </p>
-      <form action="POST">
-        <input type="email" />
-        <textarea name="" id="" cols={30} rows={10}></textarea>
+      <form action="POST" className="mt-10">
+        <input
+          type="email"
+          className="h-14 rounded-lg border border-black/10"
+        />
+        <textarea className="h-52 my-3 rounded-lg border border-black/10"></textarea>
         <button type="submit">
           Submit <IoMdSend />
         </button>
