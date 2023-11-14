6acc73acd5411fbe9233c7a4640df14f6b7192d2
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-14 16:27:44 -0500

    Adds form content and styles it

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,11 +2,13 @@
 
 import { useSectionInView } from "@/hooks/useSectionInView";
 
+import { IoMdSend } from "react-icons/io";
+
 const Contact = () => {
   const { ref } = useSectionInView("Contact", 0.5);
 
   return (
-    <section id="contact" ref={ref}>
+    <section id="contact" ref={ref} className="mb-28 sm:mb-40 scroll-mt-28">
       <p>
         Please contact me at{" "}
         <a href="mailto:m.jason.2018@gmail.com" className="underline">
@@ -14,6 +16,13 @@ const Contact = () => {
         </a>{" "}
         or through this form
       </p>
+      <form action="POST">
+        <input type="email" />
+        <textarea name="" id="" cols={30} rows={10}></textarea>
+        <button type="submit">
+          Submit <IoMdSend />
+        </button>
+      </form>
     </section>
   );
 };
