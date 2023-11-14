ee06e53375585748e403d91fb82cb403f5120840
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-14 15:22:50 -0500

    Adds mail option using a tag

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,7 +5,17 @@ import { useSectionInView } from "@/hooks/useSectionInView";
 const Contact = () => {
   const { ref } = useSectionInView("Contact", 0.5);
 
-  return <section id="contact" ref={ref}></section>;
+  return (
+    <section id="contact" ref={ref}>
+      <p>
+        Please contact me at{" "}
+        <a href="mailto:m.jason.2018@gmail.com" className="underline">
+          my email
+        </a>{" "}
+        or through this form
+      </p>
+    </section>
+  );
 };
 
 export default Contact;
