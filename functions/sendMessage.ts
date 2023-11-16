39a24deecd98afbd98cb1f5b38ff2960c99d5840
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-15 22:41:00 -0500

    Creates send email function using resend api

--- functions/sendMessage.ts
+++ functions/sendMessage.ts
@@ -1,5 +1,19 @@
 "use server";
 
-const sendMessage = async (formData: FormData) => {};
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const sendMessage = async (formData: FormData) => {
+  const senderEmail = formData.get("email");
+  const message = formData.get("message");
+
+  resend.emails.send({
+    from: senderEmail,
+    to: "m.jason.2018@gmail.com",
+    subject: "Contact Form - Portfolio",
+    text: message,
+  });
+};
 
 export default sendMessage;
