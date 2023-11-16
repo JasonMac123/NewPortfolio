c94501e852074bee287c8f94b09de2efd7eb8883
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-15 22:45:32 -0500

    Adds guard clauses if message or email is empty

--- functions/sendMessage.ts
+++ functions/sendMessage.ts
@@ -8,11 +8,28 @@ const sendMessage = async (formData: FormData) => {
   const senderEmail = formData.get("email");
   const message = formData.get("message");
 
+  if (!message || typeof message !== "string" || message.length > 500) {
+    return {
+      error: "Invalid message",
+    };
+  }
+
+  if (
+    !senderEmail ||
+    typeof senderEmail !== "string" ||
+    senderEmail.length > 100
+  ) {
+    return {
+      error: "Invalid email",
+    };
+  }
+
   resend.emails.send({
-    from: senderEmail,
+    from: "onboarding@resend.dev",
     to: "m.jason.2018@gmail.com",
     subject: "Contact Form - Portfolio",
     text: message,
+    reply_to: senderEmail,
   });
 };
 
