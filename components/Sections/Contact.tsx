483eddc2326e15039b4c6d3acfccb0d677faa997
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-18 17:35:07 -0500

    Adjusts padding and margin, and adds background color

--- components/Sections/Contact.tsx
+++ components/Sections/Contact.tsx
@@ -17,7 +17,7 @@ const Contact = () => {
     <motion.section
       id="contact"
       ref={ref}
-      className="mb-28 sm:mb-40 scroll-mt-28 w-[min(38rem,_95%)] text-center"
+      className="bg-[#DAEDBD] w-full py-8 px-12 scroll-mt-28 text-center"
       initial={{
         opacity: 0,
       }}
@@ -37,40 +37,42 @@ const Contact = () => {
         </a>{" "}
         or through this form
       </p>
-      <form
-        action={async (formData) => {
-          const result = await sendMessage(formData);
-          if (result === "Successful") {
-            toast.success("Message sent");
-          } else {
-            toast.error("Something went wrong");
-          }
-        }}
-        className="mt-10 flex flex-col"
-      >
-        <input
-          type="email"
-          name="email"
-          required
-          maxLength={500}
-          className="h-14 px-4 rounded-lg border border-black/10"
-          placeholder="Your email"
-        />
-        <textarea
-          className="h-60 my-3 rounded-lg border border-black/10 p-4 resize-none"
-          name="message"
-          required
-          maxLength={500}
-          placeholder="Your message"
-        ></textarea>
-        <button
-          type="submit"
-          className="self-end group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:bg-gray-950 hover:scale-110 active:scale-105"
+      <div className="flex flex-col items-center justify-center">
+        <form
+          action={async (formData) => {
+            const result = await sendMessage(formData);
+            if (result === "Successful") {
+              toast.success("Message sent");
+            } else {
+              toast.error("Something went wrong");
+            }
+          }}
+          className="mt-10 flex flex-col w-[min(40rem,_95%)]"
         >
-          Submit{" "}
-          <IoMdSend className="text-sm opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
-        </button>
-      </form>
+          <input
+            type="email"
+            name="email"
+            required
+            maxLength={500}
+            className="h-14 px-4 rounded-lg border border-black/10"
+            placeholder="Your email"
+          />
+          <textarea
+            className="h-60 my-3 rounded-lg border border-black/10 p-4 resize-none"
+            name="message"
+            required
+            maxLength={500}
+            placeholder="Your message"
+          ></textarea>
+          <button
+            type="submit"
+            className="self-end group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:bg-gray-950 hover:scale-110 active:scale-105"
+          >
+            Submit{" "}
+            <IoMdSend className="text-sm opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
+          </button>
+        </form>
+      </div>
     </motion.section>
   );
 };
