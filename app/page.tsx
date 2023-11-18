2105e708a11d76b3dc1f5e3c93baace5b3008328
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-18 17:34:44 -0500

    Removes small horizontal padding for webpage

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Skills from "@/components/Sections/Skills";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center px-4">
+    <main className="flex flex-col items-center">
       <Profile />
       <About />
       <ProjectList />
