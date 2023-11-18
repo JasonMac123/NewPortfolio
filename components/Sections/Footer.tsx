ff6e73694a1fca83550788aed09bdbf714556191
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-18 17:03:29 -0500

    Adds styling to footer component

--- components/Sections/Footer.tsx
+++ components/Sections/Footer.tsx
@@ -1,8 +1,15 @@
 const Footer = () => {
   return (
-    <footer>
-      <small>&copy; 2023 Jason. All rights reserved</small>
-      <p>This website was made with react, typescript,tailwind, and next.js</p>
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">
+        &copy; 2023 Jason. All rights reserved
+      </small>
+      <p className="text-xs">
+        This website was made with{" "}
+        <span className="font-semibold">
+          react, typescript, tailwind, and next.js
+        </span>
+      </p>
     </footer>
   );
 };
