7b8dd8a51cae3f1cb4e4594b176c0681790ffd43
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-18 17:12:44 -0500

    Moves submit button to the right

--- components/Sections/Contact.tsx
+++ components/Sections/Contact.tsx
@@ -65,7 +65,7 @@ const Contact = () => {
         ></textarea>
         <button
           type="submit"
-          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:bg-gray-950 hover:scale-110 active:scale-105"
+          className="self-end group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:bg-gray-950 hover:scale-110 active:scale-105"
         >
           Submit{" "}
           <IoMdSend className="text-sm opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
