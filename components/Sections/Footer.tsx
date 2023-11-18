b0b46e158495495787f182f5279ceaa438213e5d
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-18 17:01:02 -0500

    Creates footer component

---
+++ components/Sections/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer>
+      <small>&copy; 2023 Jason. All rights reserved</small>
+      <p>This website was made with react, typescript,tailwind, and next.js</p>
+    </footer>
+  );
+};
+
+export default Footer;
