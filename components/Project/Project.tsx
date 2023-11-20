16b271fb7889e2304388cd640bcd1a05bbb27902
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-20 15:23:17 -0500

    Increases project framework tag size to become more readable

--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -37,14 +37,14 @@ const Project = ({ title, description, tags, image, link }: ProjectProps) => {
         className="bg-gray-100 max-w-[42rem] cursor-pointer rounded-lg border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 group-even:pl-8 transition"
         onClick={redirectToProject}
       >
-        <div className="pt-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:w-1/2 flex flex-col h-full group-even:ml-[21rem]">
+        <div className="pt-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:w-1/2 flex flex-col h-full group-even:ml-[19rem]">
           <h3 className="text-2xl font-semibold text-[#0077b6]">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, i) => {
               return (
                 <li
-                  className="bg-[#0077b6]/[0.7] px-3 py-2 text-[0.5rem] uppercase tracking-wider text-black rounded-3xl"
+                  className="bg-[#0077b6]/[0.7] px-3 py-2 text-[0.7rem] uppercase tracking-wider text-black rounded-3xl"
                   key={i}
                 >
                   {tag}
